c6c56b44160bcf88aea4d14da215e96d83b3c223
KJH <daeding7@naver.com>
2021-12-13 19:26:32 +0900

    메인 화면이 BaseActivity 기반으로 동작하도록 설정.

--- app/src/main/java/com/kim/pizzaorderapp_kim2/MainActivity.kt
+++ app/src/main/java/com/kim/pizzaorderapp_kim2/MainActivity.kt
@@ -3,9 +3,23 @@ package com.kim.pizzaorderapp_kim2
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupEvents()
+        setValues()
     }
+    override fun setupEvents() {
+
+//        버튼 클릭 등의 이벤트 관련 코드 모음
+
+    }
+    override fun setValues() {
+
+//        화면에 데이터를 표기하기 위한 코드 모음
+    }
+
+
 }
