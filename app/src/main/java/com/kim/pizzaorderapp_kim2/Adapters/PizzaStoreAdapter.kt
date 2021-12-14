02f59830127d275a29ee3fa4a77061a124c6e279
KJH <daeding7@naver.com>
2021-12-14 20:42:08 +0900

    피자 가게 목록 어댑터 뼈대 작업

---
+++ app/src/main/java/com/kim/pizzaorderapp_kim2/Adapters/PizzaStoreAdapter.kt
@@ -0,0 +1,32 @@
+package com.kim.pizzaorderapp_kim2.Adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import com.kim.pizzaorderapp_kim2.Datas.Store
+import com.kim.pizzaorderapp_kim2.R
+import java.util.zip.Inflater
+
+class PizzaStoreAdapter(
+    val mContext : Context,
+    val resId : Int,
+    val mList : List<Store>) : ArrayAdapter<Store>(mContext, resId, mList) {
+
+        val inflater = LayoutInflater.from(mContext)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var tempRow = convertView
+        if (tempRow == null){
+            tempRow = inflater.inflate(R.layout.pizza_store_list_item,null)
+
+        }
+
+        val row = tempRow!!
+
+        return row
+    }
+
+
+}
