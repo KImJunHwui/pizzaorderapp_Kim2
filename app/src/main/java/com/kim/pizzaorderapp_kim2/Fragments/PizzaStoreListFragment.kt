97f98a73f9659f291994bf7e650a9c460b3944c5
KJH <daeding7@naver.com>
2021-12-15 19:56:04 +0900

    실제 데이터 추가 + 어댑터 / 리스트뷰 연결

--- app/src/main/java/com/kim/pizzaorderapp_kim2/Fragments/PizzaStoreListFragment.kt
+++ app/src/main/java/com/kim/pizzaorderapp_kim2/Fragments/PizzaStoreListFragment.kt
@@ -5,9 +5,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.kim.pizzaorderapp_kim2.Adapters.PizzaStoreAdapter
+import com.kim.pizzaorderapp_kim2.Datas.Store
 import com.kim.pizzaorderapp_kim2.R
+import kotlinx.android.synthetic.main.fragment_pizza_store_list.*
 
 class PizzaStoreListFragment : Fragment() {
+
+    val mPizzaStoreDataList = ArrayList<Store>()
+
+    lateinit var mPizzaStoreAdapter : PizzaStoreAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -21,6 +28,17 @@ class PizzaStoreListFragment : Fragment() {
         super.onActivityCreated(savedInstanceState)
 
 //        동작 관련 코드
+
+        mPizzaStoreDataList.add(Store("준휘 피자 가게","1111-5555", "https://cdn3.iconfinder.com/data/icons/street-food-and-food-trucker-1/64/pizza-fast-food-bake-bread-128.png"))
+        mPizzaStoreDataList.add(Store("수연 피자 가게","2222-5555", "https://cdn1.iconfinder.com/data/icons/cartoon-snack/128/pizza-128.png"))
+        mPizzaStoreDataList.add(Store("지율 피자 가게","3333-5555", "https://cdn3.iconfinder.com/data/icons/food-set-3/91/Food_C219-128.png"))
+        mPizzaStoreDataList.add(Store("응삼 피자 가게","4444-5555", "https://cdn0.iconfinder.com/data/icons/fastfood-31/64/pizza-italian-food-fast-fastfood-cheese-piece-128.png"))
+
+        mPizzaStoreAdapter = PizzaStoreAdapter(requireContext(), R.layout.pizza_store_list_item,mPizzaStoreDataList)
+
+           pizzaStoreListView.adapter = mPizzaStoreAdapter
+
+
     }
 
     }
