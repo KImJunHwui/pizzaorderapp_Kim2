670c694a7b447285d58765401d154887dfd80749
KJH <daeding7@naver.com>
2021-12-15 20:17:30 +0900

    피자 가게 목록 완성

--- app/src/main/java/com/kim/pizzaorderapp_kim2/Adapters/PizzaStoreAdapter.kt
+++ app/src/main/java/com/kim/pizzaorderapp_kim2/Adapters/PizzaStoreAdapter.kt
@@ -5,6 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import com.bumptech.glide.Glide
 import com.kim.pizzaorderapp_kim2.Datas.Store
 import com.kim.pizzaorderapp_kim2.R
 import java.util.zip.Inflater
@@ -24,6 +27,15 @@ class PizzaStoreAdapter(
         }
 
         val row = tempRow!!
+
+        val data = mList[position]
+
+        val storeNameTxt = row.findViewById<TextView>(R.id.storeNameTxt)
+        val logoImg = row.findViewById<ImageView>(R.id.logoImg)
+
+        storeNameTxt.text = data.name
+
+        Glide.with(mContext).load(data.logoUrl).into(logoImg)
 
         return row
     }
