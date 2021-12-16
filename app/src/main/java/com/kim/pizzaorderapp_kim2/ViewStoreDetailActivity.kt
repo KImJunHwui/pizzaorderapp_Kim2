97e43b70e1680a5871b614e07f07f3035a78f171
KJH <daeding7@naver.com>
2021-12-16 20:30:50 +0900

    상세 화면에 실제 데이터 반영

--- app/src/main/java/com/kim/pizzaorderapp_kim2/ViewStoreDetailActivity.kt
+++ app/src/main/java/com/kim/pizzaorderapp_kim2/ViewStoreDetailActivity.kt
@@ -2,8 +2,13 @@ package com.kim.pizzaorderapp_kim2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
+import com.kim.pizzaorderapp_kim2.Datas.Store
+import kotlinx.android.synthetic.main.activity_view_store_detail.*
 
 class ViewStoreDetailActivity : BaseActivity() {
+
+    lateinit var mStoreData : Store
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,6 +23,16 @@ class ViewStoreDetailActivity : BaseActivity() {
     }
 
     override fun setValues() {
+
+
+        mStoreData = intent.getSerializableExtra("storeData") as Store
+
+//        with 작성시 여러가지 중에 View 선택해야 함
+
+        Glide.with(mContext).load(mStoreData.logoUrl).into(storeLogoImg)
+        storeNameTxt.text = mStoreData.name
+        storePhoneNumTxt.text = mStoreData.phoneNum
+
 
     }
 
