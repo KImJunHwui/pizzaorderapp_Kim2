22da1ac024afd160a7e900188e2f3e3500d3fb5d
KJH <daeding7@naver.com>
2021-12-19 10:39:02 +0900

    치킨 가게 목록 완성(치킨가게 목록표시2)

--- app/src/main/java/com/kim/pizzaorderapp_kim2/Adapters/ChickenStoreAdapter.kt
+++ app/src/main/java/com/kim/pizzaorderapp_kim2/Adapters/ChickenStoreAdapter.kt
@@ -5,6 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import com.bumptech.glide.Glide
 import com.kim.pizzaorderapp_kim2.Datas.PizzaStore
 import com.kim.pizzaorderapp_kim2.R
 
@@ -22,6 +25,16 @@ class ChickenStoreAdapter(
         }
 
         val row = tempRow!!
+
+        val data = mList[position]
+
+        val storeNameTxt2 = row.findViewById<TextView>(R.id.storeNameTxt2)
+        val logoImg2 = row.findViewById<ImageView>(R.id.logoImg2)
+
+        storeNameTxt2.text = data.name
+
+        Glide.with(mContext).load(data.logoURL).into(logoImg2)
+
         return row
     }
 }
--- app/src/main/java/com/kim/pizzaorderapp_kim2/Fragments/ChickenStoreListFragment.kt
+++ app/src/main/java/com/kim/pizzaorderapp_kim2/Fragments/ChickenStoreListFragment.kt
@@ -5,9 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.kim.pizzaorderapp_kim2.Adapters.ChickenStoreAdapter
+import com.kim.pizzaorderapp_kim2.Adapters.PizzaStoreAdapter
+import com.kim.pizzaorderapp_kim2.Datas.PizzaStore
 import com.kim.pizzaorderapp_kim2.R
+import kotlinx.android.synthetic.main.fragment_chicken_store_list.*
 
 class ChickenStoreListFragment : Fragment() {
+
+    lateinit var mChickenStoreAdapter : ChickenStoreAdapter
+
+    val mChickenStoreDataList = ArrayList<PizzaStore>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -21,6 +29,16 @@ class ChickenStoreListFragment : Fragment() {
         super.onActivityCreated(savedInstanceState)
 
 //        동작 관련 코드
+
+        mChickenStoreDataList.add( PizzaStore("준휘 통닭가게","3456-4992", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTtuD3FDLZX5CtGrKe6UD6bGj_jh0KCL4_a2g&usqp=CAU"))
+        mChickenStoreDataList.add( PizzaStore("수연 통닭가게","1111-2222", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQAx9jVj6j4GFwdOPAFO_XCUv2u-WHPV1CgGg&usqp=CAU"))
+        mChickenStoreDataList.add( PizzaStore("지율 통닭가게","3333-4444", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQb3VZk5UuuzMHHth1V3PX3Q8l-8DEtdwa4rw&usqp=CAU"))
+        mChickenStoreDataList.add( PizzaStore("응삼 통닭가게","6666-5555", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTz-xb1Ze1Fu1Suh8U-r90wdz6YnxnV6xgXtg&usqp=CAU"))
+
+        mChickenStoreAdapter = ChickenStoreAdapter(requireContext(), R.layout.chicken_store_list_item, mChickenStoreDataList)
+
+        chickenStoreListView.adapter = mChickenStoreAdapter
+
     }
 
     }
