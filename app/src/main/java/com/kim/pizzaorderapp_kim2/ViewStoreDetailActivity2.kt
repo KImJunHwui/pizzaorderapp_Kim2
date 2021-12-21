b4685430304ad01eebbbf85aa1d93ce841ee92d1
KJH <daeding7@naver.com>
2021-12-21 21:30:40 +0900

    TedPermission 활용 -> 전화 권한 획득 후 전화 걸기

--- app/src/main/java/com/kim/pizzaorderapp_kim2/ViewStoreDetailActivity2.kt
+++ app/src/main/java/com/kim/pizzaorderapp_kim2/ViewStoreDetailActivity2.kt
@@ -1,8 +1,14 @@
 package com.kim.pizzaorderapp_kim2
 
+import android.Manifest
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.bumptech.glide.Glide
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.normal.TedPermission
 import com.kim.pizzaorderapp_kim2.Datas.PizzaStore
 import kotlinx.android.synthetic.main.activity_view_store_detail2.*
 
@@ -19,6 +25,31 @@ class ViewStoreDetailActivity2 : BaseActivity() {
 
     override fun setupEvents() {
 
+        callPhoneBtn2.setOnClickListener {
+
+            val permissionListener = object : PermissionListener{
+                override fun onPermissionGranted() {
+                    val myUri = Uri.parse("tel:${mStoreData2.phoneNum}")
+                    val myIntent = Intent(Intent.ACTION_CALL, myUri)
+                    startActivity(myIntent)
+
+                }
+
+                override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
+
+                    Toast.makeText(mContext, "전화연결 권한이 없습니다.", Toast.LENGTH_SHORT).show()
+                }
+
+            }
+
+            TedPermission.create()
+                .setPermissionListener(permissionListener)
+                .setDeniedMessage("[설정]에서 전화 권한을 켜 주세요")
+                .setPermissions(Manifest.permission.CALL_PHONE)
+                .check()
+
+
+        }
     }
 
     override fun setValues() {
